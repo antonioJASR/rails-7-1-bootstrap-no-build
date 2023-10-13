02edbd31bf76c40a4ea702bea04fc3dce403f8d7
Antonio Sánchez <antonio@bloq.dev>
2023-10-12 19:01:03 -0600

    Import Boostrap js files to the project

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,7 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
+// link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link application.css
+//= link bootstrap.min.js
+//= link popper.js
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import {Turbo} from "@hotwired/turbo-rails"
+
+import "@popperjs/core"
+import "bootstrap"
+
 import "controllers"
+
+// It works
+Turbo.setProgressBarDelay(100);
