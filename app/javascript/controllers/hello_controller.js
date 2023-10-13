9a2f815999d80fbd604fd126e31923f2d41ceb91
Antonio Sánchez <antonio@bloq.dev>
2023-10-12 19:01:47 -0600

    Add a Stimulus controller as an example

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ['modal'];
   connect() {
-    this.element.textContent = "Hello World!"
   }
+
+  sayHi() {
+    new bootstrap.Modal(this.modalTarget, {
+      backdrop: 'static'
+    }).show();
+  }
+
 }
